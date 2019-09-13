219d5105218bc2a24f2e69a256ce3c29e5b8837d
stan-kempachi <stan.bruyere@gmail.com>
2019-09-13 15:21:14 +0200

    déplacement static_url settings.py pour déployement heroku

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -1,11 +1,9 @@
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
-
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -66,6 +64,7 @@ TEMPLATE_DIRS = (
 )
 
 WSGI_APPLICATION = 'plateforme_project.wsgi.application'
+
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
@@ -113,8 +112,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
-STATIC_URL = '/pbeurre/static/'
 
 INTERNAL_IPS = [
     '127.0.0.1',
