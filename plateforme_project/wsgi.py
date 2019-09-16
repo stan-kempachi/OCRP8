a882225e46360be4b86b51e9fcb290116d3e381a
stan-kempachi <stan.bruyere@gmail.com>
2019-09-16 10:55:09 +0200

    rename project

--- plateforme_project/wsgi.py
+++ plateforme_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for plateforme_project project.
+WSGI config for OCRP8 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,5 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'plateforme_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OCRP8.settings')
 application = get_wsgi_application()
