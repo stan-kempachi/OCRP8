3609b7c50163df03e397b7d32372e5daff439554
stan-kempachi <stan.bruyere@gmail.com>
2019-09-19 14:19:28 +0200

    debug setting.py

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -18,7 +18,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pbeurre.apps.PbeurreConfig',
+    'pbeurre.app.PbeurreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
