cd6dda5f9388b0a2ccc4d3fdd2bf2e108e32ae3d
stan-kempachi <stan.bruyere@gmail.com>
2019-09-19 16:14:52 +0200

    _pycache_ dossier supprimé = run server ok

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -2,7 +2,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -115,3 +115,6 @@ USE_TZ = True
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+import django_heroku
+django_heroku.settings(locals())
