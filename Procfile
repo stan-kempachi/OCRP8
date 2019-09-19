4a606b612d93421424d932eb97cf62cfa1b7a08e
stan-kempachi <stan.bruyere@gmail.com>
2019-09-20 00:20:23 +0200

    debug procfile_

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
+web: gunicorn plateforme_project.wsgi
 web: gunicorn pbeurre:app
+
--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -115,3 +116,5 @@ USE_TZ = True
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+django_heroku.settings(locals())
