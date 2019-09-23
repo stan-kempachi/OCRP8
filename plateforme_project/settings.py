5d4d246e0b7d91b8ca9bd94cb4691cd8fcdac8e9
stan-kempachi <stan.bruyere@gmail.com>
2019-09-23 17:38:14 +0200

    settings.py STATIC_ROOT= staticfiles

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -3,7 +3,7 @@ import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
