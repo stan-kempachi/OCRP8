df55e2841fb7d3e5f1eedbd67c132a8531a97ea9
stan-kempachi <stan.bruyere@gmail.com>
2019-09-24 20:41:11 +0200

    mise en production host settings debug error 400

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -122,7 +122,14 @@ INTERNAL_IPS = [
 ]
 
 if os.environ.get('ENV') == 'PRODUCTION':
+    # Static files settings
+    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # Extra places for collectstatic to find static files.
+    STATICFILES_DIRS = (
+        os.path.join(PROJECT_ROOT, 'static'),
+    )
     STATIC_URL = '/staticfiles/'
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
