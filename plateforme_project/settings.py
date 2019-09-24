5e12ec3af84f39ba406b6cbefa0991648b4ef8ca
stan-kempachi <stan.bruyere@gmail.com>
2019-09-24 20:24:58 +0200

    mise en production

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -2,8 +2,10 @@ import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -122,5 +124,5 @@ INTERNAL_IPS = [
 if os.environ.get('ENV') == 'PRODUCTION':
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
     STATIC_URL = '/staticfiles/'
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
+    # db_from_env = dj_database_url.config(conn_max_age=500)
+    # DATABASES['default'].update(db_from_env)
