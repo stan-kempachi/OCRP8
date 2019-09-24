625971c5059e4da5cc8f8686808c38e26d6d366b
stan-kempachi <stan.bruyere@gmail.com>
2019-09-24 15:51:01 +0200

    debug static files

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -9,6 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
