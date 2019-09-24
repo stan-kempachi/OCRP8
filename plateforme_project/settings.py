99d2efefdd0cc0e92b6590bb8a1c10b3ddbffbf0
stan-kempachi <stan.bruyere@gmail.com>
2019-09-24 15:39:36 +0200

    debug static files

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -1,8 +1,15 @@
 import os
+
 import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
