8fc744fa7f7d55ab2ddb788dc417387e854b72eb
stan-kempachi <stan.bruyere@gmail.com>
2019-09-24 15:34:12 +0200

    remove django-heroku

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -1,6 +1,5 @@
 import os
 import dj_database_url
-import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
@@ -138,4 +137,3 @@ if os.environ.get('ENV') == 'PRODUCTION':
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
 
-django_heroku.settings(locals())
