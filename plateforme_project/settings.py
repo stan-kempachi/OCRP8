404373a6c620cd5ecb8f35f693ddd5831955e4b8
stan-kempachi <stan.bruyere@gmail.com>
2019-09-24 15:25:18 +0200

    static files

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -2,7 +2,7 @@ import os
 import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
