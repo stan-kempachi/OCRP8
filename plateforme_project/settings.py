1adba5181fffc6b04a4b787eb3578a29c160f366
stan-kempachi <stan.bruyere@gmail.com>
2019-09-25 14:47:03 +0200

    mise en production debug static files add django-heroku

--- plateforme_project/settings.py
+++ plateforme_project/settings.py
@@ -1,5 +1,9 @@
 import os
+
 import dj_database_url
+
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -138,3 +142,5 @@ if os.environ.get('ENV') == 'PRODUCTION':
 
     db_from_env = dj_database_url.config(conn_max_age=500)
     DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
