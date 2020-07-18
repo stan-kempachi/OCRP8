bfc9ccdb246e1b1bde67ab63d06407ed21b79795
Stan <stan.bruyere01@gmail.com>
2020-07-18 22:40:43 +0200

    new files for better organize settings

--- plateforme_project/settings/__init__.py
+++ plateforme_project/settings/__init__.py
@@ -1,30 +1,21 @@
 import os
 
-import dj_database_url
-
-import django_heroku
-
 os.environ['DJANGO_SETTINGS_MODULE'] = 'plateforme_project.settings'
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '4bf8zx8t^se760vf#$sm^p_%j=*i=nccqjb#kp(2ug+6e51_(*')
+SECRET_KEY = "k=*?viYl)?\x0czx'r)inQhsSpo>x6xQ'H[V\x0cTvZqz_d!>:b2Qv"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if os.environ.get('ENV') == 'PRODUCTION':
-   DEBUG = False
-else:
-   DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'pbeurre.herokuapp.com']
+DEBUG = True
+
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -128,32 +119,25 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+INTERNAL_IPS = ['127.0.0.1']
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': ['127.0.0.1:8080', 'pbeurre.herokuapp.com']
-    }
-}
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# LOGIN_REDIRECT_URL = 'pbeurre:search'
+STATIC_URL = '/static/'
+
 
 LOGIN_URL = 'pbeurre:login'
 
 
-if os.environ.get('ENV') == 'PRODUCTION':
 
-    # Static files settings
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
 
-django_heroku.settings(locals())
+
+
+
+
+
+
+
