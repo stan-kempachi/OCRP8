f0bda8405e0eeb944e7dc3240492c39a917b779f
Stan <stan.bruyere01@gmail.com>
2020-07-29 13:01:11 +0000

    debug

--- plateforme_project/settings/__init__.py
+++ plateforme_project/settings/__init__.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-INTERNAL_IPS = ['127.0.0.1']
+INTERNAL_IPS = []
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
